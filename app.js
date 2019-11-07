7cc4c5103a44943f661103b28b7d69619dfa040d
JaspreetKaberwal <57005350+JaspreetKaberwal@users.noreply.github.com>
2019-11-07 11:44:28 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./APP_SERVER/routes/index');
-var usersRouter = require('./APP_SERVER/routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
